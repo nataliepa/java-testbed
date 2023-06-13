84cbe41c39599995801e5ffb93af0ee5eb4e2d8b
nataliepa <nataliepanagou@gmail.com>
2023-06-13 22:02:43 +0300

    Hello World v 0.2

--- src/gr/aueb/elearn/ch1/HelloWorld.java
+++ src/gr/aueb/elearn/ch1/HelloWorld.java
@@ -1,5 +1,9 @@
 package gr.aueb.elearn.ch1;
 
+/**
+ * Prints Hello World
+ * @author N. Panagou
+ */
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
